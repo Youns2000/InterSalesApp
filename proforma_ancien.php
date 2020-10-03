67196be7936dee4e829cf669fa525402f8dd424f
Youns2000 <56587811+Youns2000@users.noreply.github.com>
2020-10-03 18:43:19 +0200

    03_10_18:43

--- proforma_ancien.php
+++ proforma_ancien.php
@@ -722,77 +722,9 @@
                           </div>
                         </div>
                       </div>
-                      <div class="col-lg-4">
-                        <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-lg btn-block btn-primary">Envoyer</button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="myModal" role="dialog">
-                          <div class="modal-dialog modal-dialog-centered">
-                          
-                            <!-- Modal content-->
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                              </div>
-                              <div class="modal-body">
-                                <?php
-                                if(isset($_SESSION['EmailResp1']) and $_SESSION['EmailResp1']!=""){ ?>
-                                <p><input type="checkbox" value="boxmailemail1" id="boxmail_email1" name="boxmail_email1"><?php echo $_SESSION['EmailResp1'] ?></p>
-                                <?php
-                                }
-                                if(isset($_SESSION['EmailResp2']) and $_SESSION['EmailResp2']!=""){?>
-                                <p><input type="checkbox" value="boxmailemail2" id="boxmail_email2" name="boxmail_email2"><?php echo $_SESSION['EmailResp2'] ?></p>
-                                <?php }?>
-                                <p><input type="checkbox" value="boxmail1" id="boxmail_1" name="boxmail_1">Azeddine@integral.fr</p>
-                                <p><input type="checkbox" value="boxmail2" id="boxmail_2" name="boxmail_2"> <input id="text2" name="text2" type="text" /></p>
-                                <p><input type="checkbox" value="boxmail3" id="boxmail_3" name="boxmail_3"> <input id="text3" name="text3" type="text" /></p>
-                                <p><input type="checkbox" value="boxmail4" id="boxmail_4" name="boxmail_4"> <input id="text4" name="text4" type="text" /></p>
-                              </div>
-                              <div class="modal-footer">
-                                <button name = "envoyermail" type="submit" class="btn btn-default">Envoyer</button>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- Modal -->
-                      </div>
-                      <div class="col-lg-4">
-                        <button type="button" data-toggle="modal" data-target="#myModal_enr" class="btn btn-lg btn-block btn-primary">Enregistrer</button>
-                        <div class="modal fade" id="myModal_enr" role="dialog">
-                          <div class="modal-dialog modal-dialog-centered">
-                          
-                            <!-- Modal content-->
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                              </div>
-                              <div class="modal-body">
-                                <select class="custom-select d-block w-100" id="getPortsEnr" name="getPortsEnr">
-                                  <option selected value>Choisir le port de destination...</option>
-                                  <?php
-                                  for ($i=0; $categ[$i]['Ports']!=""; $i++) {
-                                  ?>
-                                  <option value = "<?php echo $categ[$i]['Ports']?>"><?php echo $categ[$i]['Ports']?></option>
-                                <?php }?>
-                                </select>
-                                <select class="custom-select d-block w-100" id="getDeviseEnr" name="getDeviseEnr">
-                                  <option selected value>Choisir la devise du proforma...</option>
-                                  <?php
-                                  for ($i=0; $categ[$i]['Devises']!=""; $i++) {
-                                  ?>
-                                  <option value = "<?php echo $categ[$i]['Devises']?>"><?php echo $categ[$i]['Devises']?></option>
-                                <?php }?>
-                                </select>
-                                <input id="datevalidite2" class="form-control"  name="datevalidite2" type="date" />
-                                <input id="delailivraison2" class="form-control" name="delailivraison2" type="number" />
-                              </div>
-                              <div class="modal-footer">
-                                <button name = "enregistrer" type="submit" class="btn btn-default">OK</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+
+                      
+                      
                     </div>
                   </div>
                   </form>
---
+++ proforma_visu.php
@@ -0,0 +1,241 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['email'])){
+    header('Location: index.php');
+  } 
+
+   $sql_data='SELECT email , mdp , nom , prenom , Statut FROM comptes WHERE email = ? ORDER BY id;';
+  
+    $data=array();
+    $db = include 'db_mysql.php';
+    try { 
+      if(isset($_POST['modifmdp'])){
+              $db = include 'db_mysql.php';
+                $mail = $_SESSION['email'];
+                $stmt = $db->prepare($sql_data);
+                $stmt->execute(array($mail));
+                $data= $stmt->fetchAll(PDO::FETCH_ASSOC);
+                unset($db);
+                if(count($data)==1 and password_verify($_POST['mdpactu'], $data[0]['mdp']) and $_POST['newmdp']==$_POST['confirm']){
+                  $dbco = include 'db_mysql.php';
+                  $sth = $dbco->prepare('UPDATE comptes SET mdp=? WHERE email=?');
+                  $sth->execute(array(password_hash($_POST['newmdp'], PASSWORD_DEFAULT),$_SESSION['email']));
+                  unset($dbco);
+                }
+                else{
+                  echo "mdp incorect";
+                }
+            } 
+      } catch (Exception $e) {
+             print "Erreur ! " . $e->getMessage() . "<br/>";
+          }
+
+
+  ?>
+
+
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Younes Benreguieg">
+    <title>Integral Sales App</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="apple-touch-icon" href="logos/logo.JPG" sizes="180x180">
+    <link rel="icon" href="logos/logo.JPG" sizes="32x32" type="image/png">
+    <link rel="icon" href="logos/logo.JPG" sizes="16x16" type="image/png">
+    <link rel="manifest" href="favicons/manifest.json">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#563d7c">
+    <link rel="icon" href="logos/logo.JPG">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <meta name="msapplication-config" content="favicons/browserconfig.xml">
+    <meta name="theme-color" content="#563d7c">
+    <link href="css/style_menu.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+  <body>
+
+<!----------------------------------------------------------- NAVBAR-HAUT --------------------------------------------------------------------->
+   <nav class="navbar navbar-dark fixed-top bg-dark  p-0 shadow navbar-expand-md">
+          <a class="navbar col-sm-0 col-md-0 mr-0" href=""></a> 
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+          <div class="collapse navbar-collapse col-md-0" id="navbarCollapse">
+            <!-- <ul class="navbar p-0"> -->
+              <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                  <button type="button" data-backdrop="false" data-toggle="modal" data-target="#modal_modif_mdp" class="dropdown-item">Modifier mot de passe</button>
+                </div>
+              </div>
+            <!-- ///////////////////MODAL/////////////////////// -->
+              <form method="post">
+                    <div class="modal fade" id="modal_modif_mdp" role="dialog">
+                      <div class="modal-dialog modal-dialog-centered">
+                      
+                        <!-- Modal content-->
+                        <div class="modal-content">
+
+                          <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          </div>
+
+                          <div class="modal-body">
+                            <p>Mot de passe actuel : <input type="password" id="mdpactu" name="mdpactu" type="text"/></p>
+                            <p>Nouveau mot de passe : <input type="password" id="newmdp" name="newmdp" type="text"/></p>
+                            <p>confirmer mot de passe : <input type="password" id="confirm" name="confirm" type="text"/></p>
+                          </div>
+
+                          <div class="modal-footer">
+                            <button name = "modifmdp" type="submit" class="btn btn-default">OK</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                  <!-- /////////////////END_MODAL/////////////////////// -->
+                  <a class="navbar col-sm-0 col-md-4 mr-0" href=""></a> 
+                
+                  <a class="nav-link" href="marketing.php?categ=Postes%20Premium">
+                    <span data-feather="shopping-cart" align="center"></span>
+                    <br/>Marketing
+                  </a>
+                
+                <a class="nav-link" href="newClient.php">
+                  <span data-feather="users"></span>
+                  <br/>Nouveau Client
+                </a>
+                <a class="nav-link" href="projets.php?pr=0">
+                  <span data-feather="file"></span>
+                  <br/>Projets <span align="center"class="sr-only">(current)</span>
+                </a>
+                <!-- <a class="nav-link" href="bon_de_commande.php">
+                  <span data-feather="file-text"></span>
+                  <br/>Bon de Commande
+                </a> -->
+                <ul class="navbar-nav px-3">
+                <a class="nav-link" href="calendrier.php">
+                  <span data-feather="calendar"></span>
+                  <br/>Agenda
+                </a>
+                </ul>
+               <!-- <a class="nav-link" href="objectifs.php">
+                  <span data-feather="bar-chart-2"></span>
+                  <br/>Objectifs
+                </a>  -->
+                <!-- <a class="nav-link" href="rapports.php">
+                  <span data-feather="file-text"></span>
+                  <br/>Rapports
+                </a> -->
+                <a class="navbar col-sm-0 col-md-4 mr-0" href=""></a>
+                <ul class="navbar-nav px-3">
+                  <li class="nav-item text-nowrap">
+                    <br/><p style="color:#49FF00">Session Ouverte<br/>
+                    <?php echo $_SESSION['prenom'] ." ".$_SESSION['nom']?><br/>
+                    <a style="color:#FF0000" href="deconnection.php">Déconnection</a></p>
+                  </li>
+                </ul>
+          </div>
+  </nav>
+
+  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="container">
+        <iframe src="<?php echo chunk_split(base64_encode($_GET['p_tmp']))?>" width="700" height="800" align="middle"></iframe>
+        <!-- <?php echo $_SESSION['pdf_temp']?> -->
+      </br></br></br></br>
+
+        <div class="row">
+
+          <div class="col-lg-4">
+                        <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-lg btn-block btn-primary">Envoyer</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModal" role="dialog">
+                          <div class="modal-dialog modal-dialog-centered">
+                          
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              </div>
+                              <div class="modal-body">
+                                <?php
+                                if(isset($_SESSION['EmailResp1']) and $_SESSION['EmailResp1']!=""){ ?>
+                                <p><input type="checkbox" value="boxmailemail1" id="boxmail_email1" name="boxmail_email1"><?php echo $_SESSION['EmailResp1'] ?></p>
+                                <?php
+                                }
+                                if(isset($_SESSION['EmailResp2']) and $_SESSION['EmailResp2']!=""){?>
+                                <p><input type="checkbox" value="boxmailemail2" id="boxmail_email2" name="boxmail_email2"><?php echo $_SESSION['EmailResp2'] ?></p>
+                                <?php }?>
+                                <p><input type="checkbox" value="boxmail1" id="boxmail_1" name="boxmail_1">Azeddine@integral.fr</p>
+                                <p><input type="checkbox" value="boxmail2" id="boxmail_2" name="boxmail_2"> <input id="text2" name="text2" type="text" /></p>
+                                <p><input type="checkbox" value="boxmail3" id="boxmail_3" name="boxmail_3"> <input id="text3" name="text3" type="text" /></p>
+                                <p><input type="checkbox" value="boxmail4" id="boxmail_4" name="boxmail_4"> <input id="text4" name="text4" type="text" /></p>
+                              </div>
+                              <div class="modal-footer">
+                                <button name = "envoyermail" type="submit" class="btn btn-default">Envoyer</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!-- Modal -->
+                      </div>
+                      <div class="col-lg-4">
+                        <button type="button" data-toggle="modal" data-target="#myModal_enr" class="btn btn-lg btn-block btn-primary">Enregistrer</button>
+                        <div class="modal fade" id="myModal_enr" role="dialog">
+                          <div class="modal-dialog modal-dialog-centered">
+                          
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              </div>
+                              <div class="modal-body">
+                                <select class="custom-select d-block w-100" id="getPortsEnr" name="getPortsEnr">
+                                  <option selected value>Choisir le port de destination...</option>
+                                  <?php
+                                  for ($i=0; $categ[$i]['Ports']!=""; $i++) {
+                                  ?>
+                                  <option value = "<?php echo $categ[$i]['Ports']?>"><?php echo $categ[$i]['Ports']?></option>
+                                <?php }?>
+                                </select>
+                                <select class="custom-select d-block w-100" id="getDeviseEnr" name="getDeviseEnr">
+                                  <option selected value>Choisir la devise du proforma...</option>
+                                  <?php
+                                  for ($i=0; $categ[$i]['Devises']!=""; $i++) {
+                                  ?>
+                                  <option value = "<?php echo $categ[$i]['Devises']?>"><?php echo $categ[$i]['Devises']?></option>
+                                <?php }?>
+                                </select>
+                                <input id="datevalidite2" class="form-control"  name="datevalidite2" type="date" />
+                                <input id="delailivraison2" class="form-control" name="delailivraison2" type="number" />
+                              </div>
+                              <div class="modal-footer">
+                                <button name = "enregistrer" type="submit" class="btn btn-default">OK</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+        </div>
+      </div>
+    </main>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+        <script src="dashboard.js"></script>
+      </body>
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</html>
